dfacf4fd1b50fb25a59a9baec702469e456add7a
Braddinger13 <78782966+Braddinger13@users.noreply.github.com>
2022-06-09 15:14:40 -0400

    Autocomplete added

--- src/js/PokeSearch.js
+++ src/js/PokeSearch.js
@@ -1,24 +1,63 @@
-import { useState, React } from "react";
+import { useState, useEffect, React } from "react";
+import { Autocomplete, TextField, Grid } from "@mui/material";
+import axios from "axios";
+
 const PokeSearch = ({ getUrl }) => {
   const [searchName, setSearchName] = useState("");
+  const [pokeNames, setPokeNames] = useState([]);
 
   const onClick = (e) => {
-    if(searchName.length > 0){
-        getUrl(`https://pokeapi.co/api/v2/pokemon/` + searchName.toLowerCase());
+    if (searchName.length > 0) {
+      getUrl(`https://pokeapi.co/api/v2/pokemon/` + searchName.toLowerCase());
     }
   };
 
-  //Idea: add click arrows to scroll through pokemon
+  useEffect(() => {
+    const fetchPokeNames = async () => {
+      try {
+        const result = await axios(
+          `https://pokeapi.co/api/v2/pokemon?limit=898&offset=0`
+        );
+        setPokeNames(result.data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPokeNames();
+  }, []);
+
+  const newNames = pokeNames.map(getName);
+
+  function getName(item) {
+    return item.name[0].toUpperCase() + item.name.slice(1);
+  }
 
   return (
     <div>
-      <input
-        id="search"
-        type="text"
-        placeholder="Search Pokemon..."
-        value={searchName}
-        onChange={(e) => setSearchName(e.target.value)}
-      />
+      <span className="autoSearchBox">
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Autocomplete
+            id="combo-box-demo"
+            options={newNames}
+            sx={{
+              width: 250,
+              marginBottom: 1
+            }}
+            renderInput={(params) => (
+              <TextField {...params} label="Search Pokemon" />
+            )}
+            onInputChange={(event, newInputValue) => {
+              setSearchName(newInputValue);
+            }}
+          />
+        </Grid>
+      </span>
       <span> </span>
       <button className="searchBtn" onClick={onClick}>
         Search
